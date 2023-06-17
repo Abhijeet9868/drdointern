095ee4edf83fbad04849edf3776de8a201dddcb1
Somesh Singh <someshsingh298@gmail.com>
2023-06-17 18:32:53 +0530

    recaptcha added

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,9 +1,11 @@
 import { signIn } from "next-auth/react";
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import axios from "axios";
 
 import Input from "../Input";
 import Modal from "../Modal";
@@ -11,25 +13,56 @@ import Modal from "../Modal";
 const LoginModal = () => {
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [isBtnDisabled, setBtnDisabled] = useState(false);
+  const [response, setResponse] = useState({ message: "", status: "" });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
 
-      await signIn('credentials', {
+      if (!executeRecaptcha) {
+        return;
+      }
+      try {
+        const token = await executeRecaptcha();
+        if (!token) {
+          setResponse({ message: "Failed to Send!!!", status: "Failed" });
+          return;
+        }
+
+        const result = await axios.post("/api/recaptcha", {
+          token,
+        });
+        console.log(result);
+
+        if (result.data) {
+          setResponse({
+            message: result.data.message,
+            status: result.data.status,
+          });
+        }
+        setBtnDisabled(false);
+      } catch (error) {
+        console.log(error);
+        setResponse({ message: "Failed to Send!!!", status: "Failed" });
+        setBtnDisabled(false);
+      }
+
+      await signIn("credentials", {
         email,
         password,
       });
 
-      toast.success('Logged in');
+      toast.success("Logged in");
 
       loginModal.onClose();
     } catch (error) {
-      toast.error('Something went wrong');
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
@@ -38,40 +71,54 @@ const LoginModal = () => {
   const onToggle = useCallback(() => {
     loginModal.onClose();
     registerModal.onOpen();
-  }, [loginModal, registerModal])
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Input 
+      <Input
         placeholder="Email"
         onChange={(e) => setEmail(e.target.value)}
         value={email}
-        disabled={isLoading}  
+        disabled={isLoading}
       />
-      <Input 
+      <Input
         placeholder="Password"
         type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
-        disabled={isLoading} 
+        disabled={isLoading}
       />
+      <div className="text-sm">
+        <h3
+          className="subtitle-4"
+          style={{
+            color: response?.status === "Failed" ? "red" : "#24ff72",
+          }}
+        >
+          {response?.message}
+        </h3>
+      </div>
     </div>
-  )
+  );
 
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
-      <p>First time using Twitter?
-        <span 
-          onClick={onToggle} 
+      <p>
+        First time using Twitter?
+        <span
+          onClick={onToggle}
           className="
             text-white 
             cursor-pointer 
             hover:underline
           "
-          > Create an account</span>
+        >
+          {" "}
+          Create an account
+        </span>
       </p>
     </div>
-  )
+  );
 
   return (
     <Modal
@@ -85,6 +132,6 @@ const LoginModal = () => {
       footer={footerContent}
     />
   );
-}
+};
 
 export default LoginModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,34 @@
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast';
-import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-import Layout from '@/components/Layout'
-import LoginModal from '@/components/modals/LoginModal'
-import RegisterModal from '@/components/modals/RegisterModal'
-import '@/styles/globals.css'
-import EditModal from '@/components/modals/EditModal';
+import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import "@/styles/globals.css";
+import EditModal from "@/components/modals/EditModal";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Toaster />
-      <RegisterModal />
-      <LoginModal />
-      <EditModal />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
-  )
+    <GoogleReCaptchaProvider
+      reCaptchaKey={"6Lde5aYmAAAAAKjD0YJ8iEdmUJMZt1kds81meQmx"}
+      scriptProps={{
+        async: false, // optional, default to false,
+        defer: true, // optional, default to false
+        appendTo: "body", // optional, default to "head", can be "head" or "body",
+        nonce: undefined,
+      }}
+    >
+      <SessionProvider session={pageProps.session}>
+        <Toaster />
+        <RegisterModal />
+        <LoginModal />
+        <EditModal />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </GoogleReCaptchaProvider>
+  );
 }
